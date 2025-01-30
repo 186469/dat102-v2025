9488c72e6454ad64fa8c625ee0d9cb1d0b392ad7
186469 <elias.daland@gmail.com>
2025-01-30 10:18:07 +0100

    merge

--- f05-stabel-fyllinn/src/dat102/f05/app/baklengs/BaklengsMain.java
+++ f05-stabel-fyllinn/src/dat102/f05/app/baklengs/BaklengsMain.java
@@ -1,13 +1,22 @@
 package dat102.f05.app.baklengs;
 
+import dat102.f05.stabel.StabelADT;
+import dat102.f05.stabel.tabellstabel.TabellStabel;
+
 public class BaklengsMain {
 	
 	public static void main(String[] args) {
 		
 		String[] tabell = {"Anne", "Per", "Lise", "Knut"};
 		
-		//TODO fra scratch
+		StabelADT<String> stabel = new TabellStabel<>();
 		
+		for(String s : tabell) {
+			stabel.push(s);
+		}
+		while(!stabel.isEmpty()) {
+			System.out.println(stabel.pop());
+		}
 	}
 	
 }
--- f05-stabel-fyllinn/test/dat102/f05/stabel/tabellstabel/TabellStabelTest.java
+++ f05-stabel-fyllinn/test/dat102/f05/stabel/tabellstabel/TabellStabelTest.java
@@ -2,13 +2,47 @@ package dat102.f05.stabel.tabellstabel;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import dat102.f05.stabel.StabelADT;
 
 class TabellStabelTest {
 
+	// Metoder
+	// push, pop, peek, isEmpty, clear
+	
+	StabelADT<String> stabel; 
+
+	@BeforeEach
+	void opprettStabler() {
+		stabel = new TabellStabel<>();
+	}
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void nyStabelSkalVareTom() {
+		
+		assertTrue(stabel.isEmpty());
+			
+	}
+	
+	@Test
+	void leggeTilVirkerFint() {
+		
+		stabel.push("Per");		
+		assertFalse(stabel.isEmpty());
+		assertEquals("Per",stabel.peek());
+		
+	}
+	
+	@Test
+	void virkerClear() {
+		stabel.push("Per");
+		stabel.push("Pål");
+		stabel.clear();
+		assertTrue(stabel.isEmpty());
+		
 	}
 
+	
 }
